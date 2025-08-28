957cdb16f842576da12482837f55b8e97dc0da38
Aaditya514 <147561897+Aaditya514@users.noreply.github.com>
2025-08-28 23:27:47 +0530

    Scroll Text Animations

---
+++ SCROLL-TEXT-ANIMATIONS/script.js
@@ -0,0 +1,26 @@
+function marqueAnimations() {
+  window.addEventListener("wheel", (dets) => {
+    if (dets.deltaY > 0) {
+      gsap.to(".marque", {
+        transform: "translateX(-200%)",
+        repeat: -1,
+        duration: 4,
+        ease: "none",
+      });
+      gsap.to(".marque img", {
+        rotate: -180,
+      });
+    } else {
+      gsap.to(".marque", {
+        transform: "translateX(00%)",
+        repeat: -1,
+        duration: 4,
+        ease: "none",
+      });
+      gsap.to(".marque img", {
+        rotate: 0,
+      });
+    }
+  });
+}
+marqueAnimations();
